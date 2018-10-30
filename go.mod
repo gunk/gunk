ae772ac2a0840c208f47c6f1c55d62cc7ac94902
Daniel Martí <mvdan@mvdan.cc>
2018-10-30 17:57:53 +0000

    add -w testing flag
    While at it, don't require the user to run 'go install' commands for 'go
    test' to succeed. Don't run the commands as part of 'go test' either, as
    that would pollute the user's GOBIN.
    
    Instead, put the binaries inside .bin, and use them. We now gitignore
    that directory.
    
    Fixes #19.

--- go.mod
+++ go.mod
@@ -2,12 +2,13 @@ module github.com/gunk/gunk
 
 require (
 	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/golang/protobuf v1.2.0
 	github.com/grpc-ecosystem/grpc-gateway v1.5.1
 	github.com/gunk/opt v0.0.0-20181029164041-e949491322fc
-	golang.org/x/net v0.0.0-20181029044818-c44066c5c816
+	golang.org/x/net v0.0.0-20181029044818-c44066c5c816 // indirect
+	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f // indirect
 	golang.org/x/tools v0.0.0-20181030000716-a0a13e073c7b
 	google.golang.org/genproto v0.0.0-20181029155118-b69ba1387ce2
-	google.golang.org/grpc v1.16.0
 	gopkg.in/yaml.v2 v2.2.1 // indirect
 )
--- main_test.go
+++ main_test.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"flag"
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"testing"
 
 	"github.com/gunk/gunk/loader"
 )
+
+var write = flag.Bool("w", false, "overwrite testdata output files")
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	// Don't put the binaries in a temporary directory to delete, as that
+	// means we have to re-link them every single time. That's quite
+	// expensive, at around half a second per 'go test' invocation.
+	binDir, err := filepath.Abs(".bin")
+	if err != nil {
+		panic(err)
+	}
+	os.Setenv("GOBIN", binDir)
+	os.Setenv("PATH", binDir+string(filepath.ListSeparator)+os.Getenv("PATH"))
+	cmd := exec.Command("go", "install", "-ldflags=-w -s",
+		"github.com/golang/protobuf/protoc-gen-go",
+		"github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway",
+	)
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+
+	os.Exit(m.Run())
+}
 
 func TestGunk(t *testing.T) {
 	pkgs := []string{
@@ -28,6 +55,10 @@ func TestGunk(t *testing.T) {
 	}
 	if err := loader.Load("testdata", pkgs...); err != nil {
 		t.Fatal(err)
+	}
+	if *write {
+		// don't check that the output files match
+		return
 	}
 	for _, outPath := range outPaths {
 		want := orig[outPath]

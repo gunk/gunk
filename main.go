16bbe64105f19f6062aa5bba26e95783d5c33475
Kenneth Shaw <kenneth.shaw@knq.io>
2019-01-27 14:40:41 +0700

    Changing to compact template

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 var (
 	version = "v0.0.0-dev"
 
-	app = kingpin.New("gunk", "The modern frontend and syntax for Protocol Buffers.")
+	app = kingpin.New("gunk", "The modern frontend and syntax for Protocol Buffers.").UsageTemplate(kingpin.CompactUsageTemplate)
 
 	gen         = app.Command("generate", "Generate code from Gunk packages.")
 	genPatterns = gen.Arg("patterns", "patterns of Gunk packages").Strings()

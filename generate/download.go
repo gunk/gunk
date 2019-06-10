2e8fe1d1e1f14304778abb9f0d17897543f76bf4
Daniel Martí <mvdan@mvdan.cc>
2019-06-10 12:44:45 +0100

    revamp how protoc is downloaded in the tests
    First, make all the tests share a single $HOME. This is a simple way to
    ensure they all use the protoc downloaded by gunk itself, without having
    to download it once per test.
    
    This opens an issue, though; a dozen gunk processes might all try to
    download protoc into the same destination path concurrently. At best
    this wastes resources, and at worst this is racy and can corrupt the
    binary.
    
    Instead, start using go-internal/lockedfile, which allows using a write
    lock for writing a file across many processes. We can also use O_EXCL to
    succeed immediately if the file has already been written.
    
    Also reenable the download.txt test, while skipping it if using -short
    or if there's no network access. It takes over a second to run, but
    there's no better way to truly test that our code works with GitHub.
    
    Finally, start naming the protoc binary after its version, to support
    pinning specific versions of protoc in .gunkconfig in the near future.

--- generate/download.go
+++ generate/download.go
@@ -5,64 +5,55 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"regexp"
 	"runtime"
 
 	"github.com/gunk/gunk/log"
+	"github.com/rogpeppe/go-internal/lockedfile"
 )
 
-const (
-	protocGitHubRepo = "ProtocolBuffers/protobuf"
-)
+const protocGitHubRepo = "ProtocolBuffers/protobuf"
 
-// CheckOrDownloadProtoc will check the $PATH for 'protoc', if it doesn't
-// exist it checks to see if it has previously been downloaded to the
-// gunk cache, if not download protoc.
+// CheckOrDownloadProtoc downloads protoc to a cache folder, unless it's already
+// been downloaded. The user can pin a version in their config; otherwise, the
+// latest version is fetched from GitHub.
 func CheckOrDownloadProtoc() (string, error) {
-	// First check $PATH for protoc
-	if path, err := exec.LookPath("protoc"); err == nil && path != "" {
-		return path, nil
-	}
-
 	// Get the os specific cache directory.
 	cachePath, err := os.UserCacheDir()
 	if err != nil {
 		return "", err
 	}
-
-	gunkCachePath := filepath.Join(cachePath, "gunk")
-	// Make the gunk cache path.
-	if err := os.MkdirAll(gunkCachePath, 0755); err != nil {
+	cacheDir := filepath.Join(cachePath, "gunk")
+	if err := os.MkdirAll(cacheDir, 0755); err != nil {
 		return "", err
 	}
 
+	version := "latest"
+	// TODO: support specifying the version in the config
+
 	// The proto command path to use or download to.
-	protocCachePath := filepath.Join(gunkCachePath, "protoc")
+	dstPath := filepath.Join(cacheDir, fmt.Sprintf("protoc-%s", version))
 
 	// Check the cache path to see if it has been previously
 	// downloaded by gunk.
-	if _, err := os.Stat(protocCachePath); err == nil {
-		return protocCachePath, nil
+	dstFile, err := lockedfile.OpenFile(dstPath, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0775)
+	if os.IsExist(err) {
+		// already exists; nothing to do
+		return dstPath, nil
 	}
-
-	if err := downloadAndExtractProtoc(protocCachePath); err != nil {
+	if err != nil {
 		return "", err
 	}
-	log.Verbosef("downloaded protoc to %s", protocCachePath)
-	return protocCachePath, nil
-}
+	defer dstFile.Close()
 
-// downloadAndExtractProtoc will download protoc, and extract the protoc
-// binary to the specified location on disk.
-func downloadAndExtractProtoc(protocCachePath string) error {
 	url, err := protocDownloadURL(runtime.GOOS, runtime.GOARCH)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	// Download protoc since we were unable to find a usable
@@ -70,29 +61,27 @@ func downloadAndExtractProtoc(protocCachePath string) error {
 	cl := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return err
+		return "", err
 	}
 	res, err := cl.Do(req)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		return fmt.Errorf("could not retrieve %q (%d)", url, res.StatusCode)
+		return "", fmt.Errorf("could not retrieve %q (%d)", url, res.StatusCode)
 	}
 
+	// Check the contents of the zipped folder for the protoc binary.
 	b, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return err
+		return "", err
 	}
-
-	// Check the contents of the zipped folder for the protoc
-	// binary.
 	buf := bytes.NewReader(b)
 	rdr, err := zip.NewReader(buf, res.ContentLength)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	// Search in the zip download for the 'protoc' command.
@@ -103,22 +92,21 @@ func downloadAndExtractProtoc(protocCachePath string) error {
 
 		fc, err := f.Open()
 		if err != nil {
-			return err
+			return "", err
 		}
 		defer fc.Close()
 
-		b, err := ioutil.ReadAll(fc)
-		if err != nil {
-			return err
-		}
-
 		// Write protoc command to cache.
-		if err = ioutil.WriteFile(protocCachePath, b, f.FileInfo().Mode()); err != nil {
-			return err
+		if _, err := io.Copy(dstFile, fc); err != nil {
+			return "", err
 		}
-		return nil
+		if err := dstFile.Close(); err != nil {
+			return "", err
+		}
+		log.Verbosef("downloaded protoc to %s", dstPath)
+		return dstPath, nil
 	}
-	return fmt.Errorf("unable to download and extract protoc")
+	return "", fmt.Errorf("unable to download and extract protoc")
 }
 
 // githubAsset wraps asset information for a github release.
@@ -146,17 +134,11 @@ func githubLatestAssets(repo string) (string, []githubAsset, error) {
 	}
 	defer res.Body.Close()
 
-	// read
-	buf, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return "", nil, err
-	}
-
 	var release struct {
 		Name   string        `json:"name"`
 		Assets []githubAsset `json:"assets"`
 	}
-	if err = json.Unmarshal(buf, &release); err != nil {
+	if err := json.NewDecoder(res.Body).Decode(&release); err != nil {
 		return "", nil, err
 	}
 

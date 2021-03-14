92adf8ff6f8329bfb4764a95b368a439279c4e64
Kenneth Shaw <kenshaw@gmail.com>
2021-03-14 07:11:51 +0700

    Adding assets/assets.go go:generate for bundledt/gen.sh

--- assets/assets.go
+++ assets/assets.go
@@ -5,6 +5,7 @@ import (
 	"path"
 )
 
+//go:generate bundled/gen.sh
 //go:generate protoc -Ibundled/ --include_imports -ogen/google_api_annotations.fdp bundled/google/api/annotations.proto
 //go:generate protoc -Ibundled/ --include_imports -ogen/google_protobuf_empty.fdp bundled/google/protobuf/empty.proto
 //go:generate protoc -Ibundled/ --include_imports -ogen/google_protobuf_timestamp.fdp bundled/google/protobuf/timestamp.proto

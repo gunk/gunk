1e5785fb444ed6019ed831029a827b5d4fadaab8
Karel Bilek <karel.bilek@brank.as>
2021-03-15 15:14:34 +0700

    Simplify go downloader to do `go install`
    Available in go 1.16

--- generate/downloader/go.go
+++ generate/downloader/go.go
@@ -1,12 +1,8 @@
 package downloader
 
 import (
-	"fmt"
 	"os"
-	"path"
 	"path/filepath"
-	"strconv"
-	"strings"
 
 	"github.com/gunk/gunk/log"
 )
@@ -17,60 +13,26 @@ func (g Go) Name() string {
 	return "go"
 }
 
-func (g Go) Download(version string, p Paths) (string, error) {
-	repoPath, cmdBuildPath, err := g.repoPath(version)
-	if err != nil {
+func (pd Go) Download(version string, p Paths) (string, error) {
+	if err := os.MkdirAll(p.buildDir, 0o755); err != nil {
 		return "", err
 	}
-	cmdBuildPath = append([]string{p.buildDir}, cmdBuildPath...)
-	binaryDir := filepath.Join(cmdBuildPath...)
-	binaryPath := filepath.Join(binaryDir, "protoc-gen-go")
-	cmdArgs := []string{"clone", "--depth", "1", "--branch", version, repoPath, p.buildDir}
-	gitCmd := log.ExecCommand("git", cmdArgs...)
-	err = gitCmd.Run()
-	if err != nil {
-		all := "git " + strings.Join(cmdArgs, " ")
-		return "", log.ExecError(all, err)
-	}
-	// protoc-gen-go versions 1.3.0 and older do not have go modules
-	// so compilation would load new version of submodules and produce a hybrid
-	// we need to init modules
-	goModPath := path.Join(p.buildDir, "go.mod")
-	_, fErr := os.Stat(goModPath)
-	if fErr != nil {
-		if !os.IsNotExist(fErr) {
-			return "", fErr
-		}
-		goModInitCmd := log.ExecCommand("go", "mod", "init", "google.golang.org/protobuf")
-		goModInitCmd.Dir = p.buildDir
-		err = goModInitCmd.Run()
-		if err != nil {
-			all := "go mod init google.golang.org/protobuf"
-			return "", log.ExecError(all, err)
-		}
-	}
-	buildCmd := log.ExecCommand("go", "build")
-	buildCmd.Dir = binaryDir
-	err = buildCmd.Run()
-	if err != nil {
-		all := "go build"
-		return "", log.ExecError(all, err)
-	}
-	return binaryPath, nil
-}
 
-func (Go) repoPath(version string) (string, []string, error) {
-	version = strings.TrimPrefix(version, "v")
-	split := strings.Split(version, ".")
-	if len(split) != 3 {
-		return "", nil, fmt.Errorf("cannot interpret %q as version number: not 3 parts", split)
-	}
-	major, err := strconv.Atoi(split[0])
+	buildCmd := log.ExecCommand(
+		"go",
+		"install",
+		"google.golang.org/protobuf/cmd/protoc-gen-go@"+version)
+	buildCmd.Dir = p.buildDir
+	buildCmd.Env = append(buildCmd.Env,
+		"GOBIN="+p.buildDir,
+		"GOPATH="+os.Getenv("GOPATH"),
+		"HOME="+os.Getenv("HOME"),
+	)
+	err := buildCmd.Run()
 	if err != nil {
-		return "", nil, fmt.Errorf("cannot interpret %q as version number: major not a number", split)
+		all := "GOBIN=" + p.buildDir + " go install google.golang.org/protobuf/cmd/protoc-gen-go@" + version
+		return "", log.ExecError(all, err)
 	}
-	if major > 1 {
-		return `https://github.com/protocolbuffers/protobuf-go`, []string{`cmd`, `protoc-gen-go`}, nil
-	}
-	return "", nil, fmt.Errorf("unsupported version %q", version)
+
+	return filepath.Join(p.buildDir, "protoc-gen-go"), nil
 }

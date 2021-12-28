ada144ba3ca76c0aa389af1cf4f68bdb64b9a6c8
Chan Wen Xu <lutherchanpublic@gmail.com>
2021-12-28 18:19:46 +0800

    lint/unused: Handle container types
    The elements of "container types" should also be marked as used.

--- lint/unused.go
+++ lint/unused.go
@@ -2,30 +2,36 @@ package lint
 
 import (
 	"go/ast"
+	"go/types"
 
 	"github.com/gunk/gunk/loader"
 )
+
+type containerType interface {
+	Elem() types.Type
+}
 
 // lintUnused reports all unused structs and enums.
 func lintUnused(l *Linter, pkgs []*loader.GunkPackage) {
 	// Collect used entries.
 	usedDecl := make(map[string]bool)
 	for _, pkg := range pkgs {
-		addIdent := func(i *ast.Ident) {
-			// Mark the type the identifier uses as used.
-			obj := pkg.TypesInfo.Uses[i]
-			usedDecl[obj.Type().String()] = true
+		addType := func(typ types.Type) {
+			// Mark the type as used.
+			for typ != nil {
+				usedDecl[typ.String()] = true
+				parent, ok := typ.(containerType)
+				if !ok {
+					return
+				}
+				typ = parent.Elem()
+			}
 		}
 		for _, f := range pkg.GunkSyntax {
 			walk(f, func(n ast.Node) bool {
 				switch v := n.(type) {
 				case *ast.Field:
-					switch t := v.Type.(type) {
-					case *ast.Ident:
-						addIdent(t)
-					case *ast.SelectorExpr:
-						addIdent(t.Sel)
-					}
+					addType(pkg.TypesInfo.Types[v.Type].Type)
 				}
 				return true
 			})

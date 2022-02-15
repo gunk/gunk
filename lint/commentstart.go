980d3250634e45c4622e9ab6bd01d36c2fd6116e
Chan Wen Xu <lutherchanpublic@gmail.com>
2022-02-15 15:05:40 +0800

    lint: Add rules to comment linter
    Comments for types and struct fields must start with the name of the
    struct followed with "is/are" and end with a period.
    
    Example of compliant comment:
    ExampleField is an example field.
    
    Examples of non-compliant comments:
    ExampleField of a struct. (missing "is/are")
    ExampleField is an example field (missing ".")

--- lint/commentstart.go
+++ lint/commentstart.go
@@ -2,14 +2,15 @@ package lint
 
 import (
 	"go/ast"
+	"go/types"
 	"strings"
 
 	"github.com/gunk/gunk/loader"
 )
 
-// lintCommentStart reports all comments that do not start with the name of the
-// object they're describing.
-func lintCommentStart(l *Linter, pkgs []*loader.GunkPackage) {
+// lintComment reports all comments that do not start with the name of the
+// object they're describing or end with a period.
+func lintComment(l *Linter, pkgs []*loader.GunkPackage) {
 	for _, pkg := range pkgs {
 		for _, f := range pkg.GunkSyntax {
 			ast.Inspect(f, func(n ast.Node) bool {
@@ -19,13 +20,15 @@ func lintCommentStart(l *Linter, pkgs []*loader.GunkPackage) {
 				case *ast.File, *ast.GenDecl, *ast.StructType, *ast.InterfaceType, *ast.FieldList:
 					return true
 				case *ast.TypeSpec:
-					checkCommentStart(l, n, v.Name.Name, v.Doc.Text())
+					checkComment(l, n, v.Name.Name, v.Doc.Text(), true)
 					return true
 				case *ast.Field:
 					if len(v.Names) != 1 {
 						return true
 					}
-					checkCommentStart(l, n, v.Names[0].Name, v.Doc.Text())
+					typ := pkg.TypesInfo.TypeOf(v.Names[0])
+					_, isMethod := typ.(*types.Signature)
+					checkComment(l, n, v.Names[0].Name, v.Doc.Text(), !isMethod)
 					return true
 				}
 			})
@@ -33,16 +36,27 @@ func lintCommentStart(l *Linter, pkgs []*loader.GunkPackage) {
 	}
 }
 
-// checkCommentStart checks the start of a comment for a name prefix and adds a
+// checkComment checks the comment for a name prefix and dot suffix. It adds a
 // linter error otherwise.
-func checkCommentStart(l *Linter, n ast.Node, name string, comment string) {
+func checkComment(l *Linter, n ast.Node, name string, comment string, startWithIs bool) {
 	prefix := name + " "
-	if strings.HasPrefix(comment, prefix) {
+	if !strings.HasPrefix(comment, prefix) {
+		if comment == "" {
+			l.addError(n, "missing comment for %q", name)
+			return
+		}
+		l.addError(n, "comment for %q must start with %q", name, prefix)
 		return
 	}
-	if comment == "" {
-		l.addError(n, "missing comment for %q", name)
+	if !strings.HasSuffix(strings.TrimSpace(comment), ".") {
+		l.addError(n, "comment for %q must end with a period", name)
 		return
 	}
-	l.addError(n, "comment for %q must start with %q", name, prefix)
+	if !startWithIs {
+		return
+	}
+	if strings.HasPrefix(comment, prefix+"is ") || strings.HasPrefix(comment, prefix+"are ") {
+		return
+	}
+	l.addError(n, "comment for %q must start with %q", name, prefix+"is/are ")
 }
